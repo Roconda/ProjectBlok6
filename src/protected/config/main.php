f0f6d81acd8832ea4b555cf8531a7efc51b4b7eb
niek <niek@Niek>
2012-12-04 12:40:35 +0100

    URL MANAGER enabled

--- src/protected/config/main.php
+++ src/protected/config/main.php
@@ -45,7 +45,6 @@ return array(
 	        'class'=>'ext.bootstrap.components.Bootstrap', // assuming you extracted bootstrap under extensions
 	    ),
 		// uncomment the following to enable URLs in path-format
-		/*
 		'urlManager'=>array(
 			'urlFormat'=>'path',
 			'rules'=>array(
@@ -54,7 +53,6 @@ return array(
 				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
 			),
 		),
-		*/
 		'db'=>array(
 			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 		),
