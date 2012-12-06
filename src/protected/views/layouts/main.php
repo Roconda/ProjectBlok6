ab6597a2381e79f759a4885c375ba9fcb1c35883
Tim Slot <tim.slot@gmail.com>
2012-12-06 15:18:02 +0100

    Removed useless css

--- src/protected/views/layouts/main.php
+++ src/protected/views/layouts/main.php
@@ -3,21 +3,6 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-	<!-- blueprint CSS framework -->
-	
-	<!--
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/screen.css" media="screen, projection" />
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/print.css" media="print" />
-	-->
-	
-	<!--[if lt IE 8]>
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/ie.css" media="screen, projection" />
-	<![endif]-->
-
-	<!--
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css" />
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" />
-	-->
 	
 	<!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
     	<!--[if lt IE 9]>
