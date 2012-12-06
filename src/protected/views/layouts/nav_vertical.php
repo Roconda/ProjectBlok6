50a9b91b1e66c4d16c19365810b04633b16b876f
stevensnoeijen <stevensnoeijen@gmail.com>
2012-12-06 18:44:52 +0100

    links toegevoegd van (niewe) controllers

--- src/protected/views/layouts/nav_vertical.php
+++ src/protected/views/layouts/nav_vertical.php
@@ -4,6 +4,10 @@
 		'type' => 'list',
 		'items'=>array(
 			array('label'=>'Home', 'url'=>array('/site/index')),
+			array('label'=>'Traject', 'url'=>array('/traject')),
+			array('label'=>'Course', 'url'=>array('/course')),
+			array('label'=>'Course offer', 'url'=>array('/courseoffer')),
+			array('label'=>'Location', 'url'=>array('/location')),
 			array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
 			array('label'=>'User management', 'url'=>array('/user/user/admin')),
 			array('label'=>'Login', 'url'=>array('/user/login'), 'visible'=>Yii::app()->user->isGuest),
