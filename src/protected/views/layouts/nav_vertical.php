934ef8a1612f81c93172d046d757a91cd18a2b7d
stevensnoeijen <stevensnoeijen@gmail.com>
2012-12-06 18:34:59 +0100

    contact pagina verwijderd

--- src/protected/views/layouts/nav_vertical.php
+++ src/protected/views/layouts/nav_vertical.php
@@ -5,7 +5,6 @@
 		'items'=>array(
 			array('label'=>'Home', 'url'=>array('/site/index')),
 			array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
-			array('label'=>'Contact', 'url'=>array('/site/contact')),
 			array('label'=>'User management', 'url'=>array('/user/user/admin')),
 			array('label'=>'Login', 'url'=>array('/user/login'), 'visible'=>Yii::app()->user->isGuest),
 			array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/user/logout'), 'visible'=>!Yii::app()->user->isGuest)
