c3ea73c780e139eded5bec77e8cac6e149403f4c
stevensnoeijen <stevensnoeijen@gmail.com>
2012-12-06 19:00:58 +0100

    logging, info toegevoegd, dat ook alle info wordt gelogd (voor debugging)

--- src/protected/config/main.php
+++ src/protected/config/main.php
@@ -128,7 +128,7 @@ return array(
 			'routes'=>array(
 				array(
 					'class'=>'CFileLogRoute',
-					'levels'=>'error, warning',
+					'levels'=>'error, warning, info',
 				),
 				// uncomment the following to show log messages on web pages
 				/*
