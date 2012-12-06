1b7eb2b088356a0df633f35e4200e2c558da8fca
Tim Slot <tim.slot@gmail.com>
2012-12-06 21:21:00 +0100

    Minor layout fix

--- src/protected/views/layouts/nav_horizontal.php
+++ src/protected/views/layouts/nav_horizontal.php
@@ -10,18 +10,7 @@
           <div class="nav-collapse collapse">
             <ul class="nav">
               <li class="active"><a href="#">Home</a></li>
-              <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Language <b class="caret"></b></a>
-                <ul class="dropdown-menu">
-                  <li><a href="#">Action</a></li>
-                  <li><a href="#">Another action</a></li>
-                  <li><a href="#">Something else here</a></li>
-                  <li class="divider"></li>
-                  <li class="nav-header">Nav header</li>
-                  <li><a href="#">Separated link</a></li>
-                  <li><a href="#">One more separated link</a></li>
-                </ul>
-              </li>
+              <li><a href="#contact">Settings</a></li>
             </ul>
             <form class="navbar-form pull-right">
               <span class="muted small">Logged in as <?php echo Yii::app()->user->name; ?></span> <a href="#" class="navbar-link">Logout</a>
