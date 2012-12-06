b34dbe729f4443064fdb1b3044530158b083b3c6
stevensnoeijen <stevensnoeijen@gmail.com>
2012-12-06 18:46:30 +0100

    logout link fix

--- src/protected/views/layouts/nav_vertical.php
+++ src/protected/views/layouts/nav_vertical.php
@@ -11,7 +11,7 @@
 			array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
 			array('label'=>'User management', 'url'=>array('/user/user/admin')),
 			array('label'=>'Login', 'url'=>array('/user/login'), 'visible'=>Yii::app()->user->isGuest),
-			array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/user/logout'), 'visible'=>!Yii::app()->user->isGuest)
+			array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/user/user/logout'), 'visible'=>!Yii::app()->user->isGuest)
 		),
 	)); ?>
 </div>
