a3714c3b2adbb131cf4111b5ed0aa7a06456df37
Tim Slot <tim.slot@gmail.com>
2012-12-09 19:21:41 +0100

    Example data should not only use the ddcd named 'ddcd'.

--- src/protected/data/example_data.sql
+++ src/protected/data/example_data.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `ddcd` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `ddcd`;
 -- MySQL dump 10.13  Distrib 5.5.16, for Win32 (x86)
 --
 -- Host: localhost    Database: ddcd
