c20aa4e8516c40a08f8a79e1d11028e81a3bf493
Tim Slot <tim.slot@gmail.com>
2012-12-10 22:48:35 +0100

    Major GUI changes.
    See issue #8

--- src/protected/views/layouts/nav_horizontal.php
+++ src/protected/views/layouts/nav_horizontal.php
@@ -1,39 +1,32 @@
-    <div class="navbar navbar-inverse navbar-fixed-top">
-      <div class="navbar-inner">
-<div class="container">
-          <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </a>
-          <a class="brand" href="<?php echo Yii::app()->getHomeUrl(); ?>"><span class="avans-text"><?php echo CHtml::encode(Yii::app()->name); ?></span></a>
-          <div class="nav-collapse collapse">
+<?php
 
-			<?php if(Yii::app()->user->isGuest) { ?>
-                       
-	            <form class="navbar-form pull-right" action="<?php echo Yii::app()->getHomeUrl(); ?>/user/auth/login" method="post">
-	              <input class="span2" type="text" placeholder="Username" name="YumUserLogin[username]">
-	              <input class="span2" type="password" placeholder="Password" name="YumUserLogin[password]">
-	              <button type="submit" class="btn">Sign in</button>
-	            </form>
-	 		<?php }else{ ?>
-	 			
-	            <ul class="nav">							 
-	              <li><a href="<?php echo Yii::app()->getHomeUrl(); ?>">Home</a></li>
-	              <!--
-	              	@TODO maak settings view (passwd change, language change)
-	              <li><a href="#contact">Settings</a></li>
-	              -->
-	            </ul>
+$items = array(
+        array(
+            'class'=>'bootstrap.widgets.TbMenu',
+            'items'=>array(
+                array('label'=>'Home', 'url'=> Yii::app()->getHomeUrl()),
+            ),
+        ),
+    );
 
-	 			
-	            <ul class="nav pull-right">
-	              <li><a class="disabled">Logged in as <?php echo Yii::app()->user->name; ?></a></li>
-	              <li><a href="<?php echo Yii::app()->getHomeUrl(); ?>/user/user/logout">Log out</a></li>
-	            </ul>
-	   		<?php } ?>
-            
-          </div><!--/.nav-collapse -->
-        </div>
-      </div>
-    </div>
+if(Yii::app()->user->isGuest)
+	array_push($items, '<form class="navbar-form pull-right" action="'.Yii::app()->getHomeUrl().'/user/auth/login" method="post">
+							<input type="text" class="span2" name="YumUserLogin[username]" placeholder="Username">
+							<input type="text" class="span2" name="YumUserLogin[password]" placeholder="Password">
+							<button type="submit" class="btn">Sign in</button>
+						</form>');
+else
+	array_push($items, '<ul class="nav pull-right">
+	              			<li><a class="disabled">Logged in as '. Yii::app()->user->name .'</a></li>
+	              		</ul>');
+
+
+$this->widget('bootstrap.widgets.TbNavbar', array(
+    'type'=>'null', // null or 'inverse'
+    'brand'=> CHtml::encode(Yii::app()->name),
+    'brandUrl'=>'#',
+    'collapse'=>true, // requires bootstrap-responsive.css
+    'items'=> $items
+));
+
+?>
