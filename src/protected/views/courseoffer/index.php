3320737392966bbe35acc916a0ba2fae54152b8c
Tim Slot <tim.slot@gmail.com>
2012-12-11 13:20:46 +0100

    Refactored code

--- src/protected/views/courseoffer/index.php
+++ src/protected/views/courseoffer/index.php
@@ -29,25 +29,4 @@ $this->widget('bootstrap.widgets.TbGridView', array(
 ?>
 </div>
 
-<div class="row">
-	<div class="pull-right">
-		<?php
-		
-		$this->widget('bootstrap.widgets.TbButton', array(
-		    'label'=>'Create',
-		    'type'=>'null',
-		    'url' => array('create')
-		));
-		?>
-		 
-		&nbsp;
-		
-		<?php
-		$this->widget('bootstrap.widgets.TbButton', array(
-		    'label'=>'Manage',
-		    'type'=>'null',
-		    'url' => array('admin')
-		));
-		?>
-	</div>
-</div>
+<?php require_once(__DIR__.'/../components/button/create_manage.php'); ?>
