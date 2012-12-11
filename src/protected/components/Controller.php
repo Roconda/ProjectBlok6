b8bdec75a61f29d7ab0ff39a32f615aa4d69f2d0
stevensnoeijen <stevensnoeijen@gmail.com>
2012-12-11 20:40:37 +0100

    language-picker toegevoegd, moet nog worden geplaatst in de ui op een juiste plek.

--- src/protected/components/Controller.php
+++ src/protected/components/Controller.php
@@ -20,4 +20,13 @@ class Controller extends CController
 	 * for more details on how to specify this property.
 	 */
 	public $breadcrumbs=array();
+	
+	//voor language-picker zo instellen, alleen dit werkt niet voor de extentie controllers, daarom heb ik voor nu de import en setLanguage geplaatst in de nav_horizontal.php
+	//http://www.yiiframework.com/extension/language-picker
+/*	public function init()
+	{
+		Yii::import('ext.LangPick.ELangPick'); 
+		ELangPick::setLanguage();
+		parent::init();
+	}*/
 }
