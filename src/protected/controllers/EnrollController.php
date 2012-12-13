5acdb496143ba8393abb56e43b1b938acea17d81
Teun <tververs89@gmail.com>
2012-12-13 21:22:34 +0100

    nog wat extra rechten zooi

--- src/protected/controllers/EnrollController.php
+++ src/protected/controllers/EnrollController.php
@@ -28,11 +28,23 @@ class EnrollController extends Controller
 	{
 		return array(
 			array('allow', // allow authenticated user to perform the following
-				'actions'=>array('create','update','index','view'),
-				'users'=>array('@'),
+				'actions'=>array('create'),
+				'expression'=> "yii::app()->user->can('enroll_create')",
+			),
+			array('allow', // allow authenticated user to perform the following
+				'actions'=>array('index','view'),
+				'expression'=> "yii::app()->user->can('enroll_read')",
+			),
+			array('allow', // allow admin user to perform 'admin' and 'delete' actions
+				'actions'=>array('admin','update'),
+				'expression'=> "yii::app()->user->can('enroll_update')",
 			),
 			array('allow', // allow admin user to perform 'admin' and 'delete' actions
 				'actions'=>array('admin','delete'),
+				'expression'=> "yii::app()->user->can('enroll_delete')",
+			),
+			array('allow', // allow admin user to perform 'admin' and 'delete' actions
+				'actions'=>array('admin','create','view','index','update','delete'),
 				'users'=>array('admin'),
 			),
 			array('deny',  // deny all users
