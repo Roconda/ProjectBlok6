915e4b2821cea4e9bbe04a08e3f9cf4cc4734668
stevensnoeijen <stevensnoeijen@gmail.com>
2012-12-15 14:06:55 +0100

    permissies(actions) toegevoegd voor de rollen op de PermissionController.php en RoleController.php voor het wijzigen van Rollen en permissies

--- src/protected/controllers/modules/role/RoleController.php
+++ src/protected/controllers/modules/role/RoleController.php
@@ -4,6 +4,29 @@ Yii::import('application.modules.role.controllers.YumRoleController');
 * Override YumRoleController for calling the Controller::initELangPick() and overriding the #accessRules().
 */
 class RoleController extends YumRoleController {
+	public function accessRules() {
+		//add these rules before the parent::accessRules, becouse deny must be at the end.
+		return array(
+				array('allow',
+					'actions'=>array('create'),
+					'expression' => 'Yii::app()->user->can("role_create")'
+				),
+				array('allow',
+					'actions'=>array('index', 'admin', 'view'),
+					'expression' => 'Yii::app()->user->can("role_read")'
+				),
+				array('allow',
+					'actions'=>array('update'),
+					'expression' => 'Yii::app()->user->can("role_update")'
+				),
+				array('allow',
+					'actions'=>array('delete'),
+					'expression' => 'Yii::app()->user->can("role_delete")'
+				),
+			)
+			+ (array) parent::accessRules();
+	}
+
 	public function init()
 	{
 		Yii::import('application.components.Controller');
