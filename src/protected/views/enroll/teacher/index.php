d861e4fdd40d7edc53190ce9ae07373327f7fa40
Tim Slot <tim.slot@gmail.com>
2012-12-20 23:24:44 +0100

    made enroll contoller more dynamic and added ajax function to it

--- src/protected/views/enroll/teacher/index.php
+++ src/protected/views/enroll/teacher/index.php
@@ -13,29 +13,8 @@ $this->breadcrumbs=array(
 
 <div class="row">
 <?php
-/* 
-$this->widget('zii.widgets.CListView', array(
-	'dataProvider'=>$dataProvider,
-	'itemView'=>'_view',
-)); 
 
- * 
- */
-$this->widget('bootstrap.widgets.TbGridView', array(
-	'dataProvider'=>$dataProvider,
-	'type' => 'striped',
-	//'itemView'=>'_view',
-    'columns' => array(
-                array('name' => 'courseoffer.course.description', 'header' => Yii::t('enroll', 'Course')),
-		array('name' => 'courseoffer.location.description', 'header' => Yii::t('enroll', 'Location')),
-                array('name' => 'courseoffer.year', 'header' => Yii::t('enroll', 'Year')),
-                array('name' => 'courseoffer.block', 'header' => Yii::t('enroll', 'Trail')),
-                array('name' => 'courseoffer.course.required', 'header' => Yii::t('enroll', 'Required')),
-                array('name' => 'completed'),
-                array('name' => 'notes'),
-	)
-
-)); 
+require_once '_index_table.php';
 
 ?>
     
