eac7500b909a552fc87cfa310d5b7ef73ca9b650
stevensnoeijen <stevensnoeijen@gmail.com>
2012-12-31 15:00:44 +0100

    enroll bootstrap toegepast voor export en search, search werkt nog niet

--- src/protected/views/enroll/index.php
+++ src/protected/views/enroll/index.php
@@ -8,54 +8,66 @@ $this->breadcrumbs=array(
 ?>
 
 <h1>Enroll</h1>
+<hr />
 
-
-
-<div class="row">
-<?php
-/* 
-$this->widget('zii.widgets.CListView', array(
-	'dataProvider'=>$dataProvider,
-	'itemView'=>'_view',
-)); 
-
- * 
- */
-if(yii::app()->user->can('enroll_update_completed')) {
-   $templateField = '{update}';
-}
-else if(yii::app()->user->can('enroll_update')
-        || (yii::app()->user->isAdmin())) {
-    $templateField = '{update}{delete}';
-}
-$this->widget('bootstrap.widgets.TbGridView', array(
-	'dataProvider'=>$dataProvider,
-	'type' => 'striped',
-    'columns' => array(
-                array('name' => 'user.profile.firstname', 'header' => Yii::t('enroll', 'Firstname')),
-                array('name' => 'user.profile.lastname', 'header' => Yii::t('enroll', 'Lastname')),
-                array('name' => 'courseoffer.course.description', 'header' => Yii::t('enroll', 'Course')),
-				array('name' => 'courseoffer.location.description', 'header' => Yii::t('enroll', 'Location')),
-                array('name' => 'completed'),
-                array(
-					'class'=>'CButtonColumn',
-					'template'=>$templateField,
-					'buttons'=>array(
-						'update'=>array(
-							'url'=>'Yii::app()->createUrl("enroll/update", 
-							array("id"=>$data->user_id, "cid"=>$data->courseoffer_id))',
-						),
-						'delete'=>array(
-							'url'=>'Yii::app()->createUrl("enroll/delete",
-							array("id"=>$data->user_id, "cid"=>$data->courseoffer_id))',
-						),
-					)
-                )
+<?php 
+$this->beginWidget('zii.widgets.CPortlet', array(
+	'htmlOptions'=>array(
+		'class'=>''
 	)
-
-)); 
-
+));
+$this->widget('bootstrap.widgets.TbMenu', array(
+	'type'=>'pills',
+	'items'=>array(
+		array('label'=>'Create', 'icon'=>'icon-plus', 'url'=>Yii::app()->controller->createUrl('create'), 'linkOptions'=>array(), 'visible' => Yii::app()->user->can('enroll_create') || Yii::app()->user->isAdmin()),
+		array('label'=>'List', 'icon'=>'icon-th-list', 'url'=>Yii::app()->controller->createUrl('index'),'active'=>true, 'linkOptions'=>array()),
+		array('label'=>'Search', 'icon'=>'icon-search', 'url'=>'#', 'linkOptions'=>array('class'=>'search-button')),
+		array('label'=>'Export to PDF', 'icon'=>'icon-download', 'url'=>Yii::app()->controller->createUrl('GeneratePdf'), 'linkOptions'=>array('target'=>'_blank'), 'visible'=>true),
+		array('label'=>'Export to Excel', 'icon'=>'icon-download', 'url'=>Yii::app()->controller->createUrl('GenerateExcel'), 'linkOptions'=>array('target'=>'_blank'), 'visible'=>true),
+	),
+));
+$this->endWidget();
 ?>
-</div>
 
-<?php require_once(__DIR__.'/../components/button/enroll.php'); ?>
+<?php $this->widget('bootstrap.widgets.TbGridView',array(
+	'id'=>'enroll-grid',
+	'dataProvider'=>$dataProvider,
+        //'template'=>'{summary}{pager}{items}{pager}',
+	'columns'=>array(
+		array('name' => 'user.profile.firstname', 'header' => Yii::t('enroll', 'Firstname')),
+		array('name' => 'user.profile.lastname', 'header' => Yii::t('enroll', 'Lastname')),
+		array('name' => 'courseoffer.course.description', 'header' => Yii::t('enroll', 'Course')),
+		array('name' => 'courseoffer.location.description', 'header' => Yii::t('enroll', 'Location')),
+		array('name' => 'completed'),
+		array(
+            'class'=>'bootstrap.widgets.TbButtonColumn',
+			'template' => '{view} {update} {delete}',
+			'buttons' => array(
+			      'view' => array(
+					'label'=> 'View',
+					'url'=>'Yii::app()->createUrl("enroll/view", array("id"=>$data->user_id, "cid"=>$data->courseoffer_id))',
+					'options'=>array(
+						'class'=>'btn btn-small view'
+					)
+				),
+				'update' => array(
+					'label'=> 'Update',
+					'visible' => 'Yii::app()->user->can("enroll_update") || Yii::app()->user->isAdmin()',
+					'url'=>'Yii::app()->createUrl("enroll/update", array("id"=>$data->user_id, "cid"=>$data->courseoffer_id))',
+					'options'=>array(
+						'class'=>'btn btn-small update'
+					)
+				),
+				'delete' => array(
+					'label'=> 'Delete',
+					'visible' => 'Yii::app()->user->can("enroll_delete") || Yii::app()->user->isAdmin()',
+					'url'=>'Yii::app()->createUrl("enroll/delete", array("id"=>$data->user_id, "cid"=>$data->courseoffer_id))',
+					'options'=>array(
+						'class'=>'btn btn-small delete'
+					)
+				)
+			),
+            'htmlOptions'=>array('style'=>'width: 125px'),
+           )
+	),
+)); ?>
