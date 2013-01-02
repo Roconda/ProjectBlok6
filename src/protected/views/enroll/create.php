23530f45e0d54e747e568298427d148739f3ea1f
stevensnoeijen <stevensnoeijen@gmail.com>
2013-01-02 12:17:18 +0100

    create omgezet van enroll

--- src/protected/views/enroll/create.php
+++ src/protected/views/enroll/create.php
@@ -6,12 +6,26 @@ $this->breadcrumbs=array(
 	'Enroll'=>array('index'),
 	'Create',
 );
-
-$this->menu=array(
-	array('label'=>'List Enroll', 'url'=>array('index')),
-	array('label'=>'Manage Enroll', 'url'=>array('admin')),
-);
 ?>
 
-<h1>Create Enroll</h1>
+<h1>Create Course</h1>
+<hr/>
+
+<?php 
+$this->beginWidget('zii.widgets.CPortlet', array(
+	'htmlOptions'=>array(
+		'class'=>''
+	)
+));
+$this->widget('bootstrap.widgets.TbMenu', array(
+	'type'=>'pills',
+	'items'=>array(
+		array('label'=>'Create', 'icon'=>'icon-plus', 'url'=>Yii::app()->controller->createUrl('create'),'active'=>true, 'linkOptions'=>array()),
+                array('label'=>'List', 'icon'=>'icon-th-list', 'url'=>Yii::app()->controller->createUrl('index'), 'linkOptions'=>array()),
+		array('label'=>'Search', 'icon'=>'icon-search', 'url'=>'#', 'linkOptions'=>array('class'=>'search-button')),
+	),
+));
+$this->endWidget();
+?>
+
 <?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
