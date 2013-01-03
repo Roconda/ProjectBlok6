dee82f01889b4ff703de96ceb63d820925ab11d7
stevensnoeijen <stevensnoeijen@gmail.com>
2013-01-03 13:38:25 +0100

    pdf export header logo aangepast en text

--- src/protected/controllers/EnrollController.php
+++ src/protected/controllers/EnrollController.php
@@ -434,8 +434,7 @@ class EnrollController extends Controller
 		$pdf->SetTitle('Enroll Report');
 		$pdf->SetSubject('Enroll Report');
 		//$pdf->SetKeywords('example, text, report');
-		$pdf->SetHeaderData('', 0, "Report", '');
-		$pdf->SetHeaderData(PDF_HEADER_LOGO, PDF_HEADER_LOGO_WIDTH, "Example Report by ".Yii::app()->name, "");
+		$pdf->SetHeaderData('../../../../../../images/logo_Avans.jpg', PDF_HEADER_LOGO_WIDTH, 'Report by '.Yii::app()->name, "");
 		$pdf->setHeaderFont(Array('helvetica', '', 8));
 		$pdf->setFooterFont(Array('helvetica', '', 6));
 		$pdf->SetMargins(15, 18, 15);
--- src/protected/controllers/LocationController.php
+++ src/protected/controllers/LocationController.php
@@ -252,8 +252,7 @@ class LocationController extends Controller
 		$pdf->SetTitle('Location Report');
 		$pdf->SetSubject('Location Report');
 		//$pdf->SetKeywords('example, text, report');
-		$pdf->SetHeaderData('', 0, "Report", '');
-		$pdf->SetHeaderData(PDF_HEADER_LOGO, PDF_HEADER_LOGO_WIDTH, "Example Report by ".Yii::app()->name, "");
+		$pdf->SetHeaderData('../../../../../../images/logo_Avans.jpg', PDF_HEADER_LOGO_WIDTH, 'Report by '.Yii::app()->name, "");
 		$pdf->setHeaderFont(Array('helvetica', '', 8));
 		$pdf->setFooterFont(Array('helvetica', '', 6));
 		$pdf->SetMargins(15, 18, 15);
