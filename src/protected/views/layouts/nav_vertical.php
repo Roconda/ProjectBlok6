0e8f53b2cbc97eb60fbb52b15dcfdd6906098d5f
stevensnoeijen <stevensnoeijen@gmail.com>
2013-01-03 17:27:51 +0100

    user management visable gemaakt met isAdmin, dit overigens direct toegepast op alle visibles waar -username() == 'admin' stond

--- src/protected/views/layouts/nav_vertical.php
+++ src/protected/views/layouts/nav_vertical.php
@@ -6,16 +6,16 @@
 
 	if(!Yii::app()->user->isGuest) {
 		array_push($items, array('label'=>'Home', 'icon' => 'home', 'url'=>array('/dashboard')));
-		array_push($items, array('label'=>'Traject', 'icon' => 'random', 'url'=>array('/traject'), 'visible' => (yii::app()->user->can('traject_read') || (yii::app()->user->getName() == 'admin'))));
-		array_push($items, array('label'=>'Course', 'icon' => 'tag', 'url'=>array('/course'), 'visible' => yii::app()->user->can('course_read') || (yii::app()->user->getName() == 'admin')));
-		array_push($items, array('label'=>'Course offer', 'icon' => 'hand-right', 'url'=>array('/courseoffer'), 'visible' => yii::app()->user->can('courseoffer_read') || (yii::app()->user->getName() == 'admin')));
-		array_push($items, array('label'=>'Location', 'icon' => 'globe', 'url'=>array('/location'), 'visible' => yii::app()->user->can('location_read') || (yii::app()->user->getName() == 'admin')));
-        array_push($items, array('label'=>'Enrolled', 'icon' => 'globe', 'url'=>array('/enroll'), 'visible' => yii::app()->user->can('enroll_read') || (yii::app()->user->getName() == 'admin')));
-        array_push($items, array('label'=>'Assigned', 'icon' => 'globe', 'url'=>array('/assign') ,'visible' => yii::app()->user->can('assign_read') || (yii::app()->user->getName() == 'admin')));
+		array_push($items, array('label'=>'Traject', 'icon' => 'random', 'url'=>array('/traject'), 'visible' => (yii::app()->user->can('traject_read') || Yii::app()->user->isAdmin() )));
+		array_push($items, array('label'=>'Course', 'icon' => 'tag', 'url'=>array('/course'), 'visible' => yii::app()->user->can('course_read') || Yii::app()->user->isAdmin() ));
+		array_push($items, array('label'=>'Course offer', 'icon' => 'hand-right', 'url'=>array('/courseoffer'), 'visible' => yii::app()->user->can('courseoffer_read') || Yii::app()->user->isAdmin() ));
+		array_push($items, array('label'=>'Location', 'icon' => 'globe', 'url'=>array('/location'), 'visible' => yii::app()->user->can('location_read') || Yii::app()->user->isAdmin() ));
+        array_push($items, array('label'=>'Enrolled', 'icon' => 'globe', 'url'=>array('/enroll'), 'visible' => yii::app()->user->can('enroll_read') || Yii::app()->user->isAdmin() ));
+        array_push($items, array('label'=>'Assigned', 'icon' => 'globe', 'url'=>array('/assign') ,'visible' => yii::app()->user->can('assign_read') || Yii::app()->user->isAdmin() ));
         array_push($items, array('label'=>'My Enrolls', 'icon' => 'globe', 'url'=>array('/enroll'), 'visible' => yii::app()->user->can('enroll_read_own')));
         array_push($items, array('label'=>'My Assigns', 'icon' => 'globe', 'url'=>array('/assign') ,'visible' => yii::app()->user->can('assign_read_own')));
 		array_push($items, array('label' => 'GebruikersAdministratie', 'visible' => yii::app()->user->can('user_admin') ));
-		array_push($items, array('label'=>'User Management', 'url'=>array('/user/user/admin'), 'visible' => yii::app()->user->can('user_admin') ));
+		array_push($items, array('label'=>'User Management', 'url'=>array('/user/user/admin'), 'visible' => Yii::app()->user->can('user_admin') || Yii::app()->user->isAdmin() ));
 		array_push($items, '<hr>');
 		array_push($items, array('label'=>'Logout', 'icon' => 'icon-off', 'url'=>array('/user/user/logout', 'class' => 'muted')));	
 	}else
