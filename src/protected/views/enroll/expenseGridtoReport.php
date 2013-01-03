07c7afc8f3243b14c2c7ab75e3704b0ca7e13adc
stevensnoeijen <stevensnoeijen@gmail.com>
2013-01-03 16:09:07 +0100

    export fixes voor enroll

--- src/protected/views/enroll/expenseGridtoReport.php
+++ src/protected/views/enroll/expenseGridtoReport.php
@@ -1,11 +1,9 @@
 <?php if ($model !== null):?>
 <table border="1">
-
 	<tr>
 		<th width="80px"><?php echo Yii::t('enroll', 'Username'); ?></th>
- 		<th width="80px"><?php echo Yii::t('enroll', 'Trail'); ?></th>
- 		<th width="80px"><?php echo Yii::t('traject', 'Duration'); ?></th>
- 		<th width="80px"><?php echo Yii::t('enroll', 'Start date'); ?></th>
+ 		<th width="80px"><?php echo Yii::t('traject', 'Trail'); ?></th>
+ 		<th width="80px"><?php echo Yii::t('enroll', 'Location'); ?></th>
 		<th width="80px"><?php echo Yii::t('enroll', 'Completed'); ?></th>
  	</tr>
 	<?php foreach($model as $row): ?>
@@ -13,19 +11,16 @@
 		<td>
 			<?php echo $row->user->username; ?>
 		</td>
-       		<td>
-			<?php //echo $row->traject->description; ?>
+		<td>
+			<?php echo $row->courseoffer->course->description; ?>
 		</td>
-       		<td>
-			<?php //echo $row->traject->duration; ?>
-		</td>
-       		<td>
-			<?php //echo $row->startdate; ?>
+		<td>
+			<?php echo ($row->courseoffer->location == null ? '' : $row->courseoffer->location->description); ?>
 		</td>
 		<td>
 			<?php echo $row->completed; ?>
 		</td>
-       	</tr>
-     <?php endforeach; ?>
+	</tr>
+    <?php endforeach; ?>
 </table>
 <?php endif; ?>
