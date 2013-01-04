fe61cfe9fc627c5feed25783e4dbf011ac14da68
Tim Slot <tim.slot@gmail.com>
2013-01-04 23:24:45 +0100

    tuncated dashboard output

--- src/protected/views/enroll/teacher/dashboard.php
+++ src/protected/views/enroll/teacher/dashboard.php
@@ -4,12 +4,8 @@ $this->widget('bootstrap.widgets.TbGridView', array(
 	'type' => 'striped',
     'columns' => array(
                 array('name' => 'courseoffer.course.description', 'header' => Yii::t('enroll', 'Course')),
-		array('name' => 'courseoffer.location.description', 'header' => Yii::t('enroll', 'Location')),
-                array('name' => 'courseoffer.year', 'header' => Yii::t('enroll', 'Year')),
-                array('name' => 'courseoffer.block', 'header' => Yii::t('enroll', 'Trail')),
                 array('name' => 'courseoffer.course.required', 'header' => Yii::t('enroll', 'Required')),
                 array('name' => 'completed'),
-                array('name' => 'notes'),
 	)
 
 )); 
