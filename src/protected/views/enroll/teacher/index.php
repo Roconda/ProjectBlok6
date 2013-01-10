1b6f3e9616c9c6bc8d2bec1cf46b4eabfb64697e
Joeri <Joeri@Jvoet-W7p>
2013-01-10 13:34:14 +0100

    index.php gefixed naar verkeerde push Update en delete knop werken weer.

--- src/protected/views/enroll/teacher/index.php
+++ src/protected/views/enroll/teacher/index.php
@@ -2,44 +2,70 @@
 /* @var $this EnrollController */
 /* @var $dataProvider CActiveDataProvider */
 
-$this->breadcrumbs=array(
-	'Enroll',
+$this->breadcrumbs = array(
+    'Enroll',
 );
 ?>
 
-<h1><?php echo Yii::t('enroll','My Enrollments') ?></h1>
+<h1><?php echo Yii::t('enroll', 'My Enrollments') ?></h1>
 
 
 
 <div class="row">
-<?php
+    <?php
+    $this->widget('bootstrap.widgets.TbGridView', array(
+        'dataProvider' => $dataProvider,
+        'type' => 'striped',
+        'columns' => array(
+            array('name' => 'courseoffer.course.description', 'header' => Yii::t('enroll', 'Course')),
+            array('name' => 'courseoffer.location.description', 'header' => Yii::t('enroll', 'Location')),
+            array('name' => 'courseoffer.year', 'header' => Yii::t('enroll', 'Year')),
+            array('name' => 'courseoffer.block', 'header' => Yii::t('enroll', 'Trail')),
+            array('name' => 'courseoffer.course.required', 'type' => 'boolean', 'header' => Yii::t('enroll', 'Required')),
+            array('name' => 'completed', 'type' => 'raw', 'value' => 'Enroll::model()->getCompleted($data->completed)'),
+            array('name' => 'notes'),
+            array(
+                'class' => 'bootstrap.widgets.TbButtonColumn',
+                'template' => '{update} {delete}',
+                'buttons' => array(
+                    'view' => array(
+                        'label' => 'View',
+                        'url' => 'Yii::app()->createUrl("enroll/view", array("id"=>$data->user_id, "cid"=>$data->courseoffer_id))',
+                        'options' => array(
+                            'class' => 'btn btn-small view'
+                        )
+                    ),
+                    'update' => array(
+                        'label' => 'Update',
+                        'visible' => 'Yii::app()->user->can("enroll_update_own") || Yii::app()->user->can("enroll_update_completed")',
+                        'url' => 'Yii::app()->createUrl("enroll/update", array("id"=>$data->user_id, "cid"=>$data->courseoffer_id))',
+                        'options' => array(
+                            'class' => 'btn btn-small update'
+                        )
+                    ),
+                    'delete' => array(
+                        'label' => 'Delete',
+                        'visible' => 'Yii::app()->user->can("enroll_delete_own")',
+                        'url' => 'Yii::app()->createUrl("enroll/delete", array("id"=>$data->user_id, "cid"=>$data->courseoffer_id))',
+                        'options' => array(
+                            'class' => 'btn btn-small delete'
+                        )
+                    )
+                ),
+                'htmlOptions' => array('style' => 'width: 125px'),
+            )
+        ),
+    ));
+    ?>	
 
-$this->widget('bootstrap.widgets.TbGridView', array(
-	'dataProvider'=>$dataProvider,
-	'type' => 'striped',
-    'columns' => array(
-                array('name' => 'courseoffer.course.description', 'header' => Yii::t('enroll', 'Course')),
-				array('name' => 'courseoffer.location.description', 'header' => Yii::t('enroll', 'Location')),
-                array('name' => 'courseoffer.year', 'header' => Yii::t('enroll', 'Year')),
-                array('name' => 'courseoffer.block', 'header' => Yii::t('enroll', 'Trail')),
-                array('name' => 'courseoffer.course.required','type'=>'boolean', 'header' => Yii::t('enroll', 'Required')),
-                array('name' => 'completed', 'type' => 'raw', 'value' => 'Enroll::model()->getCompleted($data->completed)'),
-                array('name' => 'notes'),
-	)
-
-)); 
-
-?>
-    
-<?php 
-$id = Yii::app()->user->getId();
-$assign = $assignModel->findAll("user_id=$id");
-foreach($assign as $as) {
-      $hasTraject = $as->user_id;
-}
-if(isset($hasTraject))
-{
-    require_once(__DIR__.'/../../components/button/teacher/enroll.php');
-}
-?>
+    <?php
+    $id = Yii::app()->user->getId();
+    $assign = $assignModel->findAll("user_id=$id");
+    foreach ($assign as $as) {
+        $hasTraject = $as->user_id;
+    }
+    if (isset($hasTraject)) {
+        require_once(__DIR__ . '/../../components/button/teacher/enroll.php');
+    }
+    ?>
 </div>
