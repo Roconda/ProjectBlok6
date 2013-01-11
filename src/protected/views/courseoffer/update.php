18af3c169cb17442983fb9b137b265817f672465
stevensnoeijen <stevensnoeijen@gmail.com>
2013-01-11 20:28:39 +0100

    oude menu's vervangen met de bootstrap menu's, waar ik nog was vergeten

--- src/protected/views/courseoffer/update.php
+++ src/protected/views/courseoffer/update.php
@@ -7,15 +7,26 @@ $this->breadcrumbs=array(
 	$model->id=>array('view','id'=>$model->id),
 	'Update',
 );
-
-$this->menu=array(
-	array('label'=>Yii::t('courseOffer','List course offer'), 'url'=>array('index')),
-	array('label'=>Yii::t('courseOffer','Create course'), 'url'=>array('create')),
-	array('label'=>Yii::t('courseOffer','View course'), 'url'=>array('view', 'id'=>$model->id)),
-	array('label'=>Yii::t('courseOffer','Manage course offer'), 'url'=>array('admin')),
-);
 ?>
 
 <h1><?php echo Yii::t('courseOffer','Update course offer')?></h1>
+<hr/>
+
+<?php 
+$this->beginWidget('zii.widgets.CPortlet', array(
+	'htmlOptions'=>array(
+		'class'=>''
+	)
+));
+$this->widget('bootstrap.widgets.TbMenu', array(
+	'type'=>'pills',
+	'items'=>array(
+		array('label'=>Yii::t('main','Create'), 'icon'=>'icon-plus', 'url'=>Yii::app()->controller->createUrl('create'), 'linkOptions'=>array()),
+		array('label'=>Yii::t('main','List'), 'icon'=>'icon-th-list', 'url'=>Yii::app()->controller->createUrl('index'), 'linkOptions'=>array()),
+		array('label'=>Yii::t('main','Update'), 'icon'=>'icon-edit', 'url'=>Yii::app()->controller->createUrl('update',array('id'=>$model->id)),'active'=>true, 'linkOptions'=>array()),
+	),
+));
+$this->endWidget();
+?>
 
 <?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
--- src/protected/views/enroll/teacher/create.php
+++ src/protected/views/enroll/teacher/create.php
@@ -6,11 +6,24 @@ $this->breadcrumbs=array(
 	'Enroll'=>array('ownindex'),
 	'Create',
 );
-
-$this->menu=array(
-	array('label'=>'List Enroll', 'url'=>array('ownindex')),
-);
 ?>
 
 <h1><?php echo Yii::t('enroll', 'Enroll course'); ?></h1>
+<hr/>
+<?php 
+$this->beginWidget('zii.widgets.CPortlet', array(
+	'htmlOptions'=>array(
+		'class'=>''
+	)
+));
+$this->widget('bootstrap.widgets.TbMenu', array(
+	'type'=>'pills',
+	'items'=>array(
+		array('label'=>Yii::t('main','Create'), 'icon'=>'icon-plus', 'url'=>Yii::app()->controller->createUrl('create'), 'linkOptions'=>array()),
+		array('label'=>Yii::t('main','List'), 'icon'=>'icon-th-list', 'url'=>Yii::app()->controller->createUrl('index'), 'linkOptions'=>array()),
+	),
+));
+$this->endWidget();
+?>
+
 <?php echo $this->renderPartial('teacher/_form', array('model'=>$model)); ?>
