9599de3c8869931a249ab77793ae99e26523e441
Remi <Remi@192.168.1.102>
2013-01-12 18:20:10 +0100

    Regel weggehaald die iemand vergeten was met vertalen

--- src/protected/views/enroll/_form.php
+++ src/protected/views/enroll/_form.php
@@ -10,7 +10,6 @@
 )); ?>
      	<fieldset>
 		<legend>
-			<p class="note">Fields with <span class="required">*</span> are required.</p>
                         <p class="note"><?php echo Yii::t('main', 'Fields with <span class="required">*</span> are required.'); ?></p>
 		</legend>
 
